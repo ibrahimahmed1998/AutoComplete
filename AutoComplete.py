261049d6e9fc3081fcfe6fcecb29848dbc91338a
ibrahim ahmed <hema.1998.man@gmail.com>
2021-07-26 05:13:02 +0200

    Initial commit

---
+++ AutoComplete.py
@@ -0,0 +1,42 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType    # False Err
+import sqlite3
+import sys
+################################################
+FORM_CLASS,_ = loadUiType("AutoComp.ui")    # Don't Edit Here
+NameList = [] # Array To Get All Names From SQLITE_DB
+################################################
+class MainWindow(QMainWindow,FORM_CLASS):       ############
+    def __init__(self):                         #   Stamp  #
+        QMainWindow.__init__(self)              #          #
+        self.setupUi(self)                      ############
+        self.db()
+        self.LineEdit()
+################################################
+    def db(self):   # Database Connection #
+        self.connection = sqlite3.connect("db_file.db")
+        names = self.connection.execute("SELECT * FROM names")
+        for name in names:
+           NameList.append(name[1])
+################################################
+    def LineEdit(self): # Prepare Model To Pass it To Auto Complete Function #
+        NameLine_Edit = self.lineEdit
+        Completer = QCompleter()
+        NameLine_Edit.setCompleter(Completer)
+        model = QStringListModel()
+        Completer.setModel(model)
+        self.Auto_Compelete(model)
+################################################
+    def Auto_Compelete(self, model):  # Put Model To Pass NameList To It #
+        model.setStringList(NameList)
+################################################
+def main():
+    app = QApplication(sys.argv)
+    window=MainWindow()
+    window.show()
+    app.exec_()
+################################################
+if __name__=='__main__':
+    main()
